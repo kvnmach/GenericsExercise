7c5b922bc23a317cbf71a2902b31d6917d6ccd72
kvnmach <kvnmach@gmail.com>
2016-03-19 16:12:54 -0500

    added bool statements on TheVehicle class

---
+++ GenericsExercise/Shop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericsExercise
+{
+    public class Shop<T> where T : TheVehicle
+    {
+        public void isFilledUp(T TheVehicle)
+        {
+            TheVehicle.isFilledUp = true;
+            Console.WriteLine($" The{TheVehicle} had its gas tank filled ");
+        }
+        public void isNewOil 
+    }
+}
---
+++ GenericsExercise/TheVehicle.cs
@@ -0,0 +1,9 @@
+﻿namespace GenericsExercise
+{
+    public class TheVehicle
+    {
+        public bool isNewOil { get; set; }
+        public bool areTiresChanged { get; set; }
+        public bool isFilledUp { get; set; }
+    }
+}
